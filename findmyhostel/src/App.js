92f7839be383332220c2758e2532c8c837c07d00
Muhammad Feroz <81373520+Muhammad-Feroz@users.noreply.github.com>
2023-03-17 12:29:48 +0500

    Add files via upload

---
+++ findmyhostel/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Form from './modules/Form';
+import Main from './modules/Main';
+
+function App() {
+  return (
+    <>
+    {/* <Header/> */}
+    {/* <Main/> */}
+    <Form/>
+    </>
+  );
+}
+
+export default App;
---
+++ findmyhostel/src/modules/Main/data.js
@@ -0,0 +1,104 @@
+export const data = [
+  {
+    id: 1,
+    tag: 'Farm',
+    title: 'Luxury Apartment ocean view',
+    address: '153 Adriniana Mews Suite 247',
+    image: 'https://images.unsplash.com/photo-1616161610003-8b2b2b2b2b2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    features: {
+      beds: 4,
+      bath: 2,
+      sqft: 2100
+    },
+    price: 12340,
+    agent: {
+      name: 'Eleneor French',
+      date: '2 Days ago'
+    }
+  },
+  {
+    id: 2,
+    tag: 'Farm',
+    title: 'Luxury Apartment ocean view',
+    address: '153 Adriniana Mews Suite 247',
+    image: 'https://images.unsplash.com/photo-1616161610003-8b2b2b2b2b2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    features: {
+      beds: 4,
+      bath: 2,
+      sqft: 2100
+    },
+    price: 12340,
+    agent: {
+      name: 'Eleneor French',
+      date: '2 Days ago'
+    }
+  },
+  {
+    id: 3,
+    tag: 'Farm',
+    title: 'Luxury Apartment ocean view',
+    address: '153 Adriniana Mews Suite 247',
+    image: 'https://images.unsplash.com/photo-1616161610003-8b2b2b2b2b2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    features: {
+      beds: 4,
+      bath: 2,
+      sqft: 2100
+    },
+    price: 12340,
+    agent: {
+      name: 'Eleneor French',
+      date: '2 Days ago'
+    }
+  },
+  {
+    id: 4,
+    tag: 'Farm',
+    title: 'Luxury Apartment ocean view',
+    address: '153 Adriniana Mews Suite 247',
+    image: 'https://images.unsplash.com/photo-1616161610003-8b2b2b2b2b2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    features: {
+      beds: 4,
+      bath: 2,
+      sqft: 2100
+    },
+    price: 12340,
+    agent: {
+      name: 'Eleneor French',
+      date: '2 Days ago'
+    }
+  },
+  {
+    id: 5,
+    tag: 'Farm',
+    title: 'Luxury Apartment ocean view',
+    address: '153 Adriniana Mews Suite 247',
+    image: 'https://images.unsplash.com/photo-1616161610003-8b2b2b2b2b2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    features: {
+      beds: 4,
+      bath: 2,
+      sqft: 2100
+    },
+    price: 12340,
+    agent: {
+      name: 'Eleneor French',
+      date: '2 Days ago'
+    }
+  },
+  {
+    id: 6,
+    tag: 'Farm',
+    title: 'Luxury Apartment ocean view',
+    address: '153 Adriniana Mews Suite 247',
+    image: 'https://images.unsplash.com/photo-1616161610003-8b2b2b2b2b2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    features: {
+      beds: 4,
+      bath: 2,
+      sqft: 2100
+    },
+    price: 12340,
+    agent: {
+      name: 'Eleneor French',
+      date: '2 Days ago'
+    }
+  },
+]
